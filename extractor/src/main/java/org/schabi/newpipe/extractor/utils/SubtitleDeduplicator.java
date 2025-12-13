8b4c73e9b3fee7b216c988add862aff0dc63b1a9
TransZAllen <tree.story@outlook.com>
2025-12-13 21:33:49 +0800

    [duplicated subtitle] Rename `pathUsedByExoplayer()` to `getFileUri()`.

--- extractor/src/main/java/org/schabi/newpipe/extractor/utils/SubtitleDeduplicator.java
+++ extractor/src/main/java/org/schabi/newpipe/extractor/utils/SubtitleDeduplicator.java
@@ -76,7 +76,7 @@ public class SubtitleDeduplicator {
         int deduplicatedBefore = hasTheSubtitleBeenDeduplicatedBefore(cacheFile);
         // Yes, it has been deduplicated before.
         if (0 == deduplicatedBefore) {
-            String cacheFilePathForExoplayer = pathUsedByExoplayer(cacheFile);
+            String cacheFilePathForExoplayer = getFileUri(cacheFile);
             return cacheFilePathForExoplayer;
         }
 
@@ -278,7 +278,7 @@ public class SubtitleDeduplicator {
         return key;
     }
 
-    private static String pathUsedByExoplayer(File subtitleCacheFile) {
+    private static String getFileUri(File subtitleCacheFile) {
         String path = LOCAL_SUBTITLE_URL_PREFIX + subtitleCacheFile.getAbsolutePath();
 
         return path;
@@ -289,7 +289,7 @@ public class SubtitleDeduplicator {
                                             MediaFormat format) {
         File cacheFile = getDeduplicatedCacheFile(subtitleUrl, format);
 
-        String cacheFilePathForExoplayer = pathUsedByExoplayer(cacheFile);
+        String cacheFilePathForExoplayer = getFileUri(cacheFile);
 
         if (false == ensureItsParentDirExist(cacheFile)) {
             return null;
