9a0ea76e55aad3a24196c91da8be58e716bb51be
TransZAllen <tree.story@outlook.com>
2025-12-20 17:14:09 +0800

    [duplicated subtitle] Introduce SubtitleOrigin/State enums and normalize subtitle cache naming
    - Add SubtitleOrigin and SubtitleState enums to model subtitle type and processing state
    - Centralize subtitle cache filename generation with a clear, structured format
    - No functional change to subtitle downloading logic yet

---
+++ extractor/src/main/java/org/schabi/newpipe/extractor/utils/SubtitleOrigin.java
@@ -0,0 +1,22 @@
+package org.schabi.newpipe.extractor.utils;
+
+import javax.annotation.Nonnull;
+//import androidx.annotation.NonNull;
+
+public enum SubtitleOrigin {
+
+    UPLOADED("uploaded"),
+    AUTO_GENERATED("auto_generated"),
+    AUTO_TRANSLATED("auto_translated");
+
+    private final String id;
+
+    SubtitleOrigin(@Nonnull String id) {
+        this.id = id;
+    }
+
+    @Nonnull
+    public String getId() {
+        return id;
+    }
+}
---
+++ extractor/src/main/java/org/schabi/newpipe/extractor/utils/SubtitleState.java
@@ -0,0 +1,21 @@
+package org.schabi.newpipe.extractor.utils;
+
+import javax.annotation.Nonnull;
+//import androidx.annotation.NonNull;
+
+public enum SubtitleState {
+
+    ORIGINAL("original"),
+    DEDUPLICATED("deduplicated");
+
+    private final String id;
+
+    SubtitleState(@Nonnull String id) {
+        this.id = id;
+    }
+
+    @Nonnull
+    public String getId() {
+        return id;
+    }
+}
