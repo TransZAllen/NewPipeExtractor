603de281de34e5c09c30e5fac3c20f3fb5953ecc
TransZAllen <tree.story@outlook.com>
2026-01-14 00:21:22 +0800

    [duplicated subtitle] Add comments to clarify that the subtitle segments used are not in TTML format, but are regular Java strings.

--- extractor/src/test/java/org/schabi/newpipe/extractor/utils/SubtitleDeduplicatorTest.java
+++ extractor/src/test/java/org/schabi/newpipe/extractor/utils/SubtitleDeduplicatorTest.java
@@ -99,10 +99,13 @@ public class SubtitleDeduplicatorTest {
     @Test
     public void containsDuplicatedEntries_normalizeTabCharacter_shouldConsiderAsSpace() {
         // Note:
-        // This test verifies that tab characters (\t) are normalized
-        // as spaces during deduplication, considering the content
-        // as the same after this normalization, without modifying
-        // the original subtitle content.
+        // - This test verifies that tab characters (\t) are normalized
+        //   as spaces during deduplication, considering the content
+        //   as the same after this normalization, without modifying
+        //   the original subtitle content.
+        // - This test simulates runtime subtitle text *after XML entity decoding*.
+        //   Control characters like '\t' correspond to the '&#x9;' entity
+        //   in TTML subtitle files, which is an XML-based format.
         String input =
             "<p begin=\"00:00:01.000\" end=\"00:00:02.000\">Hello\tworld</p>\n" +
             "<p begin=\"00:00:01.000\" end=\"00:00:02.000\">Hello world</p>";
